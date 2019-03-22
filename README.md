58b67ca61a1c5f1cef4fe0c7ad456d4f48550da5
limengxiao <limengxiao@360.net>
2019-03-22 20:08:42 +0800

    tijiao

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 ### 个人计划项目
 
 ``` bash
-# install dependencies
+# 安装依赖
 npm install
 
-# serve with hot reload at localhost:9080
+# 项目运行 at localhost:9080
 npm run dev
 
 # build electron application for production
@@ -16,3 +16,6 @@ npm run build
 # lint all JS/Vue component files in `src/`
 npm run lint
 
+### 开发阶段备注
+
+####1、日计划中的事件 和 日历计划中的事件需要同步 初步构思是把事件都存到vuex中 或者在某个文件中读取用一套数据
--- src/renderer/pages/planDecomposition/index.vue
+++ src/renderer/pages/planDecomposition/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div class="" ref="d3mindWrapper">
       <!-- <h1>计划分解</h1> -->
       <!-- 
       <button @click="createNew()">新建</button>
@@ -12,7 +12,6 @@
 /* eslint-disable */
 import * as d3 from 'd3'
 
-<script>
 export default {
   data() {
     return {
@@ -71,31 +70,125 @@ export default {
     }
   },
   methods: {
+
+
     treeDiagram() {
-      let width = 400
-      let height = 400
+      // let width = 800
+      // let height = 600
       let _this = this
       let svg = d3.select('#d3mind')
         .append('svg')
-        .attr('width', width)
-        .attr('height', height)
+        .attr('width', 1000)
+        .attr('height', 1000)
       
       // 初始化树状图数据获取器
       let tree = d3.tree()
-        .size([width, height - 80])
+        .size([500, 500])
         .separation(function(a, b) {
           return (a.parent === b.parent ? 1 : 2) / a.depth
         })
 
-      let hierarchyData =  d3.hierarchyData(_this.treeData)
+      let hierarchyData = d3.hierarchy(_this.treeData)
         .sum(function(d) {
           return d.value
         })
+
+      // 初始化树状图
+      let treeData = tree(hierarchyData)
+      // 获取节点
+      let nodes = treeData.descendants()
+      // 获取边,也就是连线
+      let links = treeData.links()
+      // 绘制线
+      let g = svg.append('g').attr('transform', 'translate(40,0)')
+      g.selectAll('.link')
+        .data(links)
+        .enter().append('path')
+        .attr('class', 'link')
+        // .style('fill', '#cccccc')
+        .attr('d', d3.linkHorizontal()
+          .x(function (d) { return d.y })
+          .y(function (d) { return d.x }))
+      // 绘制文本和节点
+      g.selectAll('.node')
+        .data(nodes)
+        .enter().append('g')
+        .attr('class', function (d) { return 'node' + (d.children ? ' node--internal' : ' node--leaf') })
+        .attr('transform', function (d) { return 'translate(' + d.y + ',' + d.x + ')' })
+        .on('click', this.editNode)
+      g.selectAll('.node').append('circle')
+        .attr('r', 5)
+        .style('fill', 'green')
+      g.selectAll('.node').append('text')
+        .attr('dy', 3)
+        .attr('x', function (d) { return d.children ? -8 : 8 })
+        .style('text-anchor', function (d) { return d.children ? 'end' : 'start' })
+        .text(function (d) {
+          return d.data.name
+        })
+        .style('font-size', '11px')
+      
+    },
+    update() {
+
+    },
+
+    addNodes() {
+
+    },
+
+    editNode(d) {
+      console.log("点击的点。。。")
+      console.log(d)
     }
   },
-  mountd() {
+  mounted() {
     this.treeDiagram()
   }
 }
 </script>
+
+
+<style>
+#d3mind {
+  width: 100%;
+  height: 100%;
+}
+
+/* svg {
+  width: 100%;
+  height: 100%;
+} */
+
+#d3mind .every {
+  width: 400px;
+  height: 425px;
+  margin:15px;
+  float: left;
+}
+#d3mind .every h3 {
+  margin:0;
+}
+#d3mind .every h3 .button {
+  float: right;
+  margin-right: 20px;
+  font-size: 14px;
+  cursor: pointer;
+  padding: 2px 8px;
+  border:1px solid #ccc;
+  background: yellowgreen;
+  border-radius: 4px;
+}
+#d3mind .every h3 .button:hover {
+  background: violet;
+}
+#d3mind .every .svg {
+  width: 400px;
+  height: 400px;
+}
+
+#d3mind .node {
+  fill: #fff;
+}
+</style>
 
